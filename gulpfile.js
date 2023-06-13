1479a197bbc55f7de14fc602a6d423810281d1fa
JakeSCahill <jake@redpanda.com>
2023-06-13 10:55:17 +0100

    Reconfigure settings

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,7 @@ const yaml = require('js-yaml')
 const playbookFilename = 'local-antora-playbook.yml'
 const playbook = yaml.load(fs.readFileSync(playbookFilename, 'utf8'))
 const outputDir = (playbook.output || {}).dir || './build/site'
-const serverConfig = { name: 'Preview Site', livereload, port: 5000, root: outputDir }
+const serverConfig = { name: 'Preview Site', livereload, port: 5002, root: outputDir }
 const antoraArgs = ['--playbook', playbookFilename]
 const watchPatterns = playbook.content.sources.filter((source) => !source.url.includes(':')).reduce((accum, source) => {
   accum.push(`${source.url}/${source.start_path ? source.start_path + '/' : ''}antora.yml`)
